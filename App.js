ac7d03da455046b73d88e161c969a90efcfcaa83
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-05 14:53:12 +0700

    learn background gradient

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 class App extends Component {
@@ -10,27 +10,18 @@ class App extends Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
-        <TouchableOpacity>
-          <LinearGradient
-            style={{
-              paddingVertical: 30,
-              marginHorizontal: 10,
-              borderRadius: 3,
-              justifyContent: 'center',
-              alignItems: 'center',
-              elevation: 3,
-              marginTop: 20,
-            }}
-            start={{x: 0, y: 0}}
-            end={{x: 1, y: 0}}
-            colors={['#833ab4', '#fd1d1d', '#fcb045']}>
-            <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
-              Sign in
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
+      <LinearGradient
+        style={{
+          flex: 1,
+        }}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 0}}
+        colors={['#833ab4', '#fd1d1d', '#fcb045']}>
+        <StatusBar backgroundColor="transparent" translucent />
+        <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
+          Sign in
+        </Text>
+      </LinearGradient>
     );
   }
 }
