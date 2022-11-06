810b3bf25272169873cdfeb9cfd266ba8b74d749
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-06 11:14:58 +0700

    learn react native spinkit

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
 import React, {Component} from 'react';
 import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
-import Toast from 'react-native-toast-message';
+var Spinner = require('react-native-spinkit');
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  showToastNotification = () => {
-    Toast.show({
-      type: 'info',
-      text1: 'Hello',
-      text2: 'This is some something 👋',
-    });
-  };
-
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Toast />
-        <TouchableOpacity onPress={() => this.showToastNotification()}>
-          <Text>Hello World</Text>
-        </TouchableOpacity>
+        <Spinner
+          style={{marginBottom: 50}}
+          isVisible={true}
+          size={25}
+          type={'Wave'}
+          color={'#212121'}
+        />
       </View>
     );
   }
