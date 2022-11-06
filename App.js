e99d22b385bd4c12c6ced6c4e170613bc56f2d19
Chaerul Umam <chaerulumamb244@gmail.com>
2022-11-06 10:58:09 +0700

    learn react native toast message

--- App.js
+++ App.js
@@ -1,31 +1,25 @@
 import React, {Component} from 'react';
 import {StatusBar, Text, TouchableOpacity, View} from 'react-native';
-import Snackbar from 'react-native-snackbar';
+import Toast from 'react-native-toast-message';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  showNotification = () => {
-    Snackbar.show({
-      text: 'Hello World',
-      duration: Snackbar.LENGTH_SHORT,
-      action: {
-        text: 'UNDO',
-        textColor: 'green',
-        onPress: () => {
-          /* Do something. */
-          console.log('Undo clicked');
-        },
-      },
+  showToastNotification = () => {
+    Toast.show({
+      type: 'info',
+      text1: 'Hello',
+      text2: 'This is some something 👋',
     });
   };
 
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity onPress={() => this.showNotification()}>
+        <Toast />
+        <TouchableOpacity onPress={() => this.showToastNotification()}>
           <Text>Hello World</Text>
         </TouchableOpacity>
       </View>
